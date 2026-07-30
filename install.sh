2daa6f3be341e13e412fa76ec38f65dda6fe6999
LeonLab <iliundleon@outlook.de>
2026-07-30 16:20:12 +0200

    update the install.sh for better update support ;)
    Added check to remove existing fym directory before moving new binary.

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ set -e
 
 g++ -O3 -std=c++20 "$HOME/fym/main.cpp" -o "$HOME/fym/fym"
 
+ordnerpfad="/usr/local/bin/fym" 
+
+if [[ -d "$ordnerpfad" ]]; then
+  sudo rm -rf "$ordnerpfad"
+fi
+
 sudo mv ~/fym/fym /usr/local/bin/fym
 
 echo "ready to use!"
