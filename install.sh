10b6cbd3653311344a950d38d91e56c5289d162b
LeonLab <iliundleon@outlook.de>
2026-07-30 17:16:17 +0200

    fixed my own mistake

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ set -e
 
 g++ -O3 -std=c++20 "$HOME/fym/main.cpp" -o "$HOME/fym/fym"
 
-ordnerpfad="/usr/local/bin/fym" 
+dicpath="/usr/local/bin/fym" 
 
-if [[ -d "$ordnerpfad" ]]; then
-  sudo rm -rf "$ordnerpfad"
+if [[ -f "$dicpath" ]]; then
+  sudo rm -rf "$dicpath"
 fi
 
 sudo mv ~/fym/fym /usr/local/bin/fym
